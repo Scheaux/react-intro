489a011e20c9c5ca595cb5d9f8f61eff24cd8611
Scheaux <125101@inbox.ru>
2022-06-29 15:58:38 +0700

    first commit

---
+++ src/components/ShopItemClass.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ShopItemClass extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  static propTypes = {
+    item: PropTypes.object.isRequired
+  };
+
+  render() {
+    const { item } = this.props;
+    return (
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">
+          {item.descriptionFull}
+        </div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{item.currency}{item.price}.00</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShopItemClass;
